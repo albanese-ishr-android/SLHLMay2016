3610d185a61aefff5dbe8bf2a93385128e6887bc
tony-albanese <tony.albanese2000@gmail.com>
2019-11-07 14:37:11 +0100

    Initial commit.

---
+++ src/BatesMotel.java
@@ -0,0 +1,14 @@
+public class BatesMotel {
+
+
+    public static void main(String[] args) {
+
+        Client client = new Client();
+        String  name;
+        name = client.getName();
+        client.setCustomerID("12344");
+
+    }
+
+
+}
