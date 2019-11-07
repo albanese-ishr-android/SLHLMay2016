2ac3f5fd8e7948ce65ffa9e68f0a367220da1d4b
tony-albanese <tony.albanese2000@gmail.com>
2019-11-07 14:37:33 +0100

    Added comments.

--- src/Client.java
+++ src/Client.java
@@ -1,9 +1,19 @@
 public class Client {
+
+    //Member variables.
+    //This is the data for the class / object
+    //private means the variables are local (to the class)
+    //They cannot be accessed from outside the class.
+
     private int customerID;
     private String name;
     private Dates arrive;
     private Dates leave;
     private Room bedroom;
+
+    //This is the constructor. It is called whenever you make a new object.
+    //The parameters that are passed in are whatever you decide are required
+    //to make a new object.
 
     public Client(int id, String c, Dates dateIn, Dates dateOut, Room r) {
         setCustomerID(id);
@@ -13,10 +23,19 @@ public class Client {
         setBedroom(r);
     }
 
+    //Accessor and mutator methods
+    //Also known as getters and setters.
+    //These are the methods that allow you to change the variables or see their value.
     public int getCustomerID() {
         return customerID;
     }
 
+    //public means any class can access methods.
+    //The return type comes next. It tells you what type of data the function returns.
+    //The return value is kind of like output, but not exactly.
+
+
+    //void means no return type. The function does it's job and then stops.
     public void setCustomerID(int id) {
         customerID = id;
     }
@@ -25,6 +44,8 @@ public class Client {
         return name;
     }
 
+    //Inside the parentheses () are the arguements (or parameters)
+    //These come from the outside -- the things that are using this function.
     public void setName(String c) {
         name = c;
     }
