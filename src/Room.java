4b1f2e2721536608a92e3744f54551d0a2d2214e
tony-albanese <tony.albanese2000@gmail.com>
2019-12-01 10:22:42 +0100

    Initial commit.

--- src/Room.java
+++ src/Room.java
@@ -1,7 +1,17 @@
 public class Room {
 
-//roomNumber a value that identifies the room
 //beds the number of beds in the room
+public int beds;
+    //roomNumber a value that identifies the room
+    int roomNumber;
 //price the price of the room, per night
+double price;
 //empty indicating whether or not the room is occupied.
+boolean empty;
+
+    public Room() {
+        //Constructor
+    }
+
+
 }
