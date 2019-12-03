caab548f56f590f9d9af60df4d48bd1d60207997
tony-albanese <tony.albanese2000@gmail.com>
2019-12-03 13:57:35 +0100

    Added signature for method to add a Client object to a linked list.

--- src/BatesMotel.java
+++ src/BatesMotel.java
@@ -1,22 +1,16 @@
 import com.sun.jmx.remote.internal.ClientCommunicatorAdmin;
 
+import java.util.LinkedList;
+
 public class BatesMotel {
 
 
     public static void main(String[] args) {
 
-        //Create a Client object.
-        Client client = new Client(1000, "Albanese");
-
-        //Create another Client object.
-        Client secondClient = new Client(1001, "Johnson");
-
-        //Call methods on the objects.
-        client.getName();
-        secondClient.getName();
-
-
     }
 
+    public static void newClient(Client client, LinkedList<Client> bookingsList){
+        
+    }
 
 }
