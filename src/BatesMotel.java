220654d036408d5c74b4bc9ba5c3e7e4593d7d25
tony-albanese <tony.albanese2000@gmail.com>
2019-12-03 14:14:24 +0100

    Created test methods for the classes.

--- src/BatesMotel.java
+++ src/BatesMotel.java
@@ -7,6 +7,17 @@ public class BatesMotel {
 
     public static void main(String[] args) {
 
+        LinkedList<Client> list = new LinkedList<Client>();
+        Client firstClient = new Client("Albanese", 4);
+        Client secondClient = new Client("Bill Nye", 7);
+
+        newClient(firstClient, list);
+        newClient(secondClient, list);
+
+        System.out.println(list.size());
+
+
+
     }
 
     public static void newClient(Client client, LinkedList<Client> bookingsList){
@@ -14,6 +25,15 @@ public class BatesMotel {
         //If a clients are added on the same day, then the new one is added
         //after the second one.
 
+
+        //If the list is empty, just add the client to the beginning of the list.
+        if(bookingsList.isEmpty()){
+            bookingsList.addFirst(client);
+        }
+
+    }
+
+    public static void printList(LinkedList<Client> list){
         
     }
 
