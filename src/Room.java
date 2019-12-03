54d96f834f0b208332a22d058c5a773a43c86cb8
tony-albanese <tony.albanese2000@gmail.com>
2019-12-03 13:58:21 +0100

    Added method for the bill().

--- src/Room.java
+++ src/Room.java
@@ -2,16 +2,18 @@ public class Room {
 
 //beds the number of beds in the room
 public int beds;
-    //roomNumber a value that identifies the room
-    int roomNumber;
+//roomNumber a value that identifies the room
+int roomNumber;
 //price the price of the room, per night
 double price;
-//empty indicating whether or not the room is occupied.
+//boolean indicating whether or not the room is occupied.
 boolean empty;
 
-    public Room() {
+public Room() {
         //Constructor
     }
 
+public void bill(){
 
+}
 }
